fa21752e84d760d6d5c0b0a31b797a39c1d71b79
Ali Tehami <ali.tehami@woodsbagot.com>
2020-07-20 12:50:02 +1000

    Add project files.

---
+++ SampleViewExtension/SampleViewExtension.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Windows;
+
+using System.Windows.Controls;
+using Dynamo.Wpf.Extensions;
+
+
+namespace SampleViewExtension
+{
+    class SampleViewExtension : IViewExtension
+    {
+        private MenuItem sampleMenuItem;
+
+        public string UniqueId
+        {
+            get
+            {
+                return Guid.NewGuid().ToString();
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return "Sample View Extension";
+            }
+        }
+
+        public void Dispose()
+        {
+            //throw new NotImplementedException();
+        }
+
+
+        public void Loaded(ViewLoadedParams p)
+        {
+            sampleMenuItem = new MenuItem { Header = "Sample ViewExtension, AliT" };
+            sampleMenuItem.Click += (sender, args) =>
+            {
+                var viewModel = new SampleWindowViewModel(p);
+                var window = new SampleWindow
+                {
+                    MainGrid = { DataContext = viewModel },
+                    Owner = p.DynamoWindow
+                };
+                window.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+                window.Show();
+            };
+            p.AddMenuItem(MenuBarType.View, sampleMenuItem);
+        }
+
+        public void Shutdown()
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void Startup(ViewStartupParams p)
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
---
+++ SampleViewExtension/SampleWindowViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Dynamo.Extensions;
+using Dynamo.Core;
+using Dynamo.Graph.Nodes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SampleViewExtension
+{
+    public class SampleWindowViewModel
+    {
+
+        public SampleWindowViewModel(ReadyParams p)
+        {
+            return;
+        }
+
+    }
+}
