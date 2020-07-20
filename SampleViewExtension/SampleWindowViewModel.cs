6bd3ea2c1ddf7b1dc315a213421fdfcec85dc2ab
Ali Tehami <ali.tehami@woodsbagot.com>
2020-07-21 09:43:50 +1000

    setup and ready. Tested on v 2.0.x

--- SampleViewExtension/SampleWindowViewModel.cs
+++ SampleViewExtension/SampleWindowViewModel.cs
@@ -6,17 +6,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Windows.Ink;
 
 namespace SampleViewExtension
 {
-    public class SampleWindowViewModel
+    public class SampleWindowViewModel : NotificationObject, IDisposable
     {
+        private string activeNodeTypes;
+        private ReadyParams readyParams;
+
+        public string ActiveNodeTypes
+        {
+            get
+            {
+                activeNodeTypes = getNodeTypes();
+                return activeNodeTypes;
+            }
+        }
+
+        // Helper function that builds string of active nodes
+        public string getNodeTypes()
+        {
+            string output = "Active nodes:\n";
+
+            foreach (NodeModel node in readyParams.CurrentWorkspaceModel.Nodes)
+            {
+                string nickName = node.Name;
+                output += nickName + "\n";
+            }
+
+            return output;
+        }
+
 
         public SampleWindowViewModel(ReadyParams p)
         {
-            return;
+            readyParams = p;
+            p.CurrentWorkspaceModel.NodeAdded += CurrentWorkspaceModel_NodesChanged;
+            p.CurrentWorkspaceModel.NodeRemoved += CurrentWorkspaceModel_NodesChanged;
         }
 
+        private void CurrentWorkspaceModel_NodesChanged(NodeModel obj)
+        {
+            RaisePropertyChanged("ActiveNodeTypes");
+        }
+
+        public void Dispose()
+        {
+            readyParams.CurrentWorkspaceModel.NodeAdded -= CurrentWorkspaceModel_NodesChanged;
+            readyParams.CurrentWorkspaceModel.NodeRemoved -= CurrentWorkspaceModel_NodesChanged;
+        }
     }
 }
